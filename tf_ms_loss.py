ed48cc3f50016fba818ae7011ba555de5fe6405f
geonm <geonm@kaist.ac.kr>
2019-07-22 10:22:51 +0000

    first commit

---
+++ tf_ms_loss.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+
+def ms_loss(labels, embeddings, alpha=2.0, beta=50.0, lamb=1.0, eps=0.1, ms_mining=False):
+    '''
+    ref: http://openaccess.thecvf.com/content_CVPR_2019/papers/Wang_Multi-Similarity_Loss_With_General_Pair_Weighting_for_Deep_Metric_Learning_CVPR_2019_paper.pdf
+    official codes: https://github.com/MalongTech/research-ms-loss
+    '''
+    # make sure emebedding should be l2-normalized
+    embeddings = tf.nn.l2_normalize(embeddings, axis=1)
+    labels = tf.reshape(labels, [-1, 1])
+
+    batch_size = embeddings.get_shape().as_list()[0]
+
+    adjacency = tf.equal(labels, tf.transpose(labels))
+    adjacency_not = tf.logical_not(adjacency)
+
+    mask_pos = tf.cast(adjacency, dtype=tf.float32) - tf.eye(batch_size, dtype=tf.float32)
+    mask_neg = tf.cast(adjacency_not, dtype=tf.float32)
+
+    sim_mat = tf.matmul(embeddings, embeddings, transpose_a=False, transpose_b=True)
+    sim_mat = tf.maximum(sim_mat, 0.0)
+
+    pos_mat = tf.multiply(sim_mat, mask_pos)
+    neg_mat = tf.multiply(sim_mat, mask_neg)
+
+    if ms_mining:
+        max_val = tf.reduce_max(neg_mat)
+        tmp_max_val = tf.reduce_max(pos_mat)
+        min_val = tf.reduce_min(tf.multiply(sim_mat - tmp_max_val, mask_pos)) + tmp_max_val
+        pos_mat = tf.where(pos_mat < max_val + eps, pos_mat, tf.zeros_like(pos_mat))
+        neg_mat = tf.where(neg_mat > min_val - eps, neg_mat, tf.zeros_like(neg_mat))
+
+    pos_exp = tf.exp(-alpha * (pos_mat - lamb))
+    pos_exp = tf.where(mask_pos > 0.0, pos_exp, tf.zeros_like(pos_exp))
+
+    neg_exp = tf.exp(beta * (neg_mat - lamb))
+    neg_exp = tf.where(mask_neg > 0.0, neg_exp, tf.zeros_like(neg_exp))
+
+    pos_term = tf.log(1.0 + tf.reduce_sum(pos_exp)) / alpha
+    neg_term = tf.log(1.0 + tf.reduce_sum(neg_exp)) / beta
+
+    loss = tf.reduce_mean(pos_term + neg_term)
+
+    return loss
